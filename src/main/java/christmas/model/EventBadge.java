c2df7f87dbca588f73bb6f8a4819af322afa06c8
kyoo0115 <internetkyoomin@gmail.com>
2023-11-12 00:17:36 +0900

    feat: added enums for the types of badges with events

--- src/main/java/christmas/model/EventBadge.java
+++ src/main/java/christmas/model/EventBadge.java
@@ -1,4 +1,31 @@
 package christmas.model;
 
-public class EventBadge {
+public enum EventBadge {
+    NONE(0),
+    STAR(5000),
+    TREE(10000),
+    SANTA(20000)
+
+    private final int threshold;
+
+    EventBadge(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public static EventBadge getBadgeForAmount(long discountAmount) {
+        if (discountAmount >= SANTA.threshold) {
+            return SANTA;
+        }
+        if (discountAmount >= TREE.threshold) {
+            return TREE;
+        }
+        if (discountAmount >= STAR.threshold) {
+            return STAR;
+        }
+        return NONE;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
 }
