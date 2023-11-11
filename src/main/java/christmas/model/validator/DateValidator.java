17792785ac4c705ce4abbfc7d167e5976a9854f3
kyoo0115 <internetkyoomin@gmail.com>
2023-11-12 00:21:53 +0900

    feat: added validation for the date

--- src/main/java/christmas/model/validator/DateValidator.java
+++ src/main/java/christmas/model/validator/DateValidator.java
@@ -1,2 +1,16 @@
-package christmas.model.validator;public class DateValidator {
+package christmas.model.validator;
+
+public class DateValidator implements Validator<Integer>{
+    @Override
+    public Integer validate(String input) {
+        try {
+            int date = Integer.parseInt(input);
+            if (date < 1 || date > 31) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+            return date;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
+        }
+    }
 }
