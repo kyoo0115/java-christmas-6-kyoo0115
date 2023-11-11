36722f77b9e5c874de87f772787fef679eadf038
kyoo0115 <internetkyoomin@gmail.com>
2023-11-11 19:38:00 +0900

    feat: created simple structure of the project layout

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,21 @@
+package christmas.view;
+
+public class InputView {
+
+    public enum Prompt {
+        WELCOME_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
+        VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+        ORDER_LIST("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
+
+        private final String message;
+
+        Prompt(String message) {
+            this.message = message;
+        }
+
+        public void display() {
+            System.out.println(message);
+        }
+    }
+}
