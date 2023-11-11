13cba150fa367ec894bda99d7b6791689b497598
kyoo0115 <internetkyoomin@gmail.com>
2023-11-12 00:02:49 +0900

    feat: implemented controller handling inputs for validation

--- src/main/java/christmas/controller/InputController.java
+++ src/main/java/christmas/controller/InputController.java
@@ -1,4 +1,39 @@
 package christmas.controller;
 
+import christmas.model.validator.DateValidator;
+import christmas.model.validator.OrderListValidator;
+import christmas.view.InputView;
+
+import java.util.Set;
+
 public class InputController {
+
+    private final DateValidator dateValidator;
+    private final OrderListValidator orderListValidator;
+
+    public InputController(Set<String> validMenuItems) {
+        this.dateValidator = new DateValidator();
+        this.orderListValidator = new OrderListValidator(validMenuItems);
+    }
+    public int readAndValidateVisitDate() {
+        while (true) {
+            try {
+                String inputDate = InputView.readVisitDate();
+                return dateValidator.validate(inputDate);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public String readAndValidateOrderList() {
+        while (true) {
+            try {
+                String inputOrderList = InputView.readOrderList();
+                return orderListValidator.validate(inputOrderList);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
