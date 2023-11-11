3443aa478f0a8beb3218b784c30e0ea6e3cf6bc9
kyoo0115 <internetkyoomin@gmail.com>
2023-11-12 00:21:16 +0900

    feat: implemented class for menuitem for the specific type of menu

--- src/main/java/christmas/model/MenuItem.java
+++ src/main/java/christmas/model/MenuItem.java
@@ -1,4 +1,25 @@
 package christmas.model;
 
 public class MenuItem {
+    private final String name;
+    private final int price;
+    private final MenuCategory category;
+
+    public MenuItem(String name, int price, MenuCategory category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public boolean isCategory(MenuCategory otherCategory) {
+        return this.category == otherCategory;
+    }
+
+    public String displayInfo() {
+        return String.format("%s - %d원, %s", name, price, category);
+    }
+
+    public int calculateDiscountedPrice(int discountAmount) {
+        return price - discountAmount;
+    }
 }
