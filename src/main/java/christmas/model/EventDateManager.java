e25560a1a4bd71ed28843c2a028ca4b044bd2027
kyoo0115 <internetkyoomin@gmail.com>
2023-11-14 16:37:19 +0900

    feat: implemented a class to setup the dates with events

--- src/main/java/christmas/model/EventDateManager.java
+++ src/main/java/christmas/model/EventDateManager.java
@@ -1,4 +1,27 @@
 package christmas.model;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 public class EventDateManager {
+    private final Set<LocalDate> specialOfferDates;
+
+    public EventDateManager() {
+        this.specialOfferDates = new HashSet<>();
+        initializeSpecialOfferDates();
+    }
+
+    private void initializeSpecialOfferDates() {
+        specialOfferDates.add(LocalDate.of(2023, 12, 3));
+        specialOfferDates.add(LocalDate.of(2023, 12, 10));
+        specialOfferDates.add(LocalDate.of(2023, 12, 17));
+        specialOfferDates.add(LocalDate.of(2023, 12, 24));
+        specialOfferDates.add(LocalDate.of(2023, 12, 25));
+        specialOfferDates.add(LocalDate.of(2023, 12, 31));
+    }
+
+    public boolean isSpecialOfferDate(LocalDate date) {
+        return specialOfferDates.contains(date);
+    }
 }
