8385a5d7cbc7102e7872702550c03be7e611f8a9
kyoo0115 <internetkyoomin@gmail.com>
2023-11-14 16:37:42 +0900

    feat: implemented the service layer of the badge for the calculation

--- src/main/java/christmas/model/service/BadgeService.java
+++ src/main/java/christmas/model/service/BadgeService.java
@@ -1,2 +1,11 @@
-package christmas.model.service;public class BadgeService {
+package christmas.model.service;
+
+import christmas.model.EventBadge;
+import christmas.model.Order;
+
+public class BadgeService {
+    public EventBadge determineBadge(Order order) {
+        long totalBenefitAmount = order.calculateTotalBenefitAmount();
+        return EventBadge.getBadgeForAmount(totalBenefitAmount);
+    }
 }
