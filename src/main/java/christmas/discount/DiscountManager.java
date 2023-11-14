5cd15ea020ee4f0d5edf27ba09b865a8b0740c29
kyoo0115 <internetkyoomin@gmail.com>
2023-11-15 01:47:39 +0900

    feat: added discountmanager to handle discount details

--- src/main/java/christmas/discount/DiscountManager.java
+++ src/main/java/christmas/discount/DiscountManager.java
@@ -1,2 +1,17 @@
-package christmas.discount;public class DiscountManager {
+package christmas.discount;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiscountManager {
+    private long totalDiscount;
+    private final Map<String, Long> discountDetails = new HashMap<>();
+
+    public long getTotalDiscount() {
+        return totalDiscount;
+    }
+
+    public Map<String, Long> getDiscountDetails() {
+        return discountDetails;
+    }
 }
