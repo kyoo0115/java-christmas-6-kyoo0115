cd2a624fa52d4f46d74a324bfb369d47d59cc76a
kyoo0115 <internetkyoomin@gmail.com>
2023-11-14 16:38:03 +0900

    feat: implemented the service layer of the gift for the calculation

--- src/main/java/christmas/model/service/GiftService.java
+++ src/main/java/christmas/model/service/GiftService.java
@@ -1,2 +1,21 @@
-package christmas.model.service;public class GiftService {
+package christmas.model.service;
+
+import christmas.model.Order;
+import java.util.Optional;
+
+public class GiftService {
+    public static final long GIFT_ELIGIBILITY_THRESHOLD = 120_000;
+    public static final long GIFT_VALUE = 25_000;
+    public static final String GIFT_ITEM = "샴폐인";
+
+    public boolean checkGiftEligibility(Order order) {
+        long totalPriceBeforeDiscounts = order.calculateTotalPriceBeforeDiscounts();
+        boolean isEligibleGift = totalPriceBeforeDiscounts >= GIFT_ELIGIBILITY_THRESHOLD;
+        order.setGiftEligibility(isEligibleGift);
+        return isEligibleGift;
+    }
+
+    public Optional<String> getGiftItem(Order order) {
+        return order.isEligibleForGift() ? Optional.of(GIFT_ITEM) : Optional.empty();
+    }
 }
