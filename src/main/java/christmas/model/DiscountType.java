0f09d43248ea487ccc8af98d468e816cb8afdb87
kyoo0115 <internetkyoomin@gmail.com>
2023-11-14 16:36:26 +0900

    feat: implemented enum type of discount types

--- src/main/java/christmas/model/DiscountType.java
+++ src/main/java/christmas/model/DiscountType.java
@@ -1,2 +1,35 @@
-package christmas.model;public enum DiscountType {
+package christmas.model;
+
+import christmas.model.service.strategy.ChristmasDDayDiscount;
+import christmas.model.service.strategy.DiscountStrategy;
+import christmas.model.service.strategy.GiftEventDiscount;
+import christmas.model.service.strategy.SpecialDiscount;
+import christmas.model.service.strategy.WeekdayDiscount;
+import christmas.model.service.strategy.WeekendDiscount;
+import java.time.LocalDate;
+
+public enum DiscountType {
+    CHRISTMAS_D_DAY(new ChristmasDDayDiscount(), "크리스마스 디데이 할인"),
+    WEEKDAY_DISCOUNT(new WeekdayDiscount(), "평일 할인"),
+    WEEKEND_DISCOUNT(new WeekendDiscount(), "주말 할인"),
+    SPECIAL_DISCOUNT(new SpecialDiscount(), "특별 할인"),
+    GIFT_EVENT(new GiftEventDiscount(), "증정 할인");
+
+    private final DiscountStrategy strategy;
+    private final String displayName;
+
+    DiscountType(DiscountStrategy strategy, String displayName) {
+        this.strategy = strategy;
+        this.displayName = displayName;
+    }
+
+    public long calculateDiscount(Order order, LocalDate visitDate, DiscountPolicy discountPolicy, EventDateManager eventDateManager) {
+        return strategy.calculateDiscount(order, visitDate, discountPolicy, eventDateManager);
+    }
+
+    @Override
+    public String toString() {
+        return this.displayName;
+    }
 }
+
