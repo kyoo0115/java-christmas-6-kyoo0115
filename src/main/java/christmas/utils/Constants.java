24136cc7f9264c9ac326f0f5e9e58071893dfe30
kyoo0115 <internetkyoomin@gmail.com>
2023-11-14 22:38:18 +0900

    feat: added additional constants

--- src/main/java/christmas/utils/Constants.java
+++ src/main/java/christmas/utils/Constants.java
@@ -3,4 +3,7 @@ package christmas.utils;
 public class Constants {
     public static final String ITEM_QUANTITY_DELIMITER = "-";
     public static final String ORDER_DELIMITER = ",";
+    public static final int ZERO = 0;
+    public static final int START_OF_DECEMBER = 1;
+    public static final int END_OF_DECEMBER = 31;
 }
