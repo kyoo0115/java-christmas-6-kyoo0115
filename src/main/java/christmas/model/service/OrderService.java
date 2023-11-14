8e5da305479f4a15493ddfe7d0900d0cc4601191
kyoo0115 <internetkyoomin@gmail.com>
2023-11-15 01:52:42 +0900

    feat: added order service to handle order object

--- src/main/java/christmas/model/service/OrderService.java
+++ src/main/java/christmas/model/service/OrderService.java
@@ -1,2 +1,30 @@
-package christmas.model.service;public class OrderService {
+package christmas.model.service;
+
+import christmas.model.entity.MenuItem;
+import christmas.model.entity.Order;
+import christmas.model.entity.OrderEntry;
+import christmas.utils.Util;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class OrderService {
+
+    private final Set<MenuItem> menuItems;
+
+    public OrderService(Set<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+    public Order createOrderFromInput(String validatedOrderList) {
+        Order order = new Order();
+        Map<String, MenuItem> menuItemMap = Util.createMenuItemMap(menuItems);
+        List<OrderEntry> orderEntries = Util.parseOrderEntries(validatedOrderList);
+
+        for (OrderEntry entry : orderEntries) {
+            MenuItem item = menuItemMap.get(entry.itemName());
+            order.addItem(item, entry.quantity());
+        }
+        return order;
+    }
 }
