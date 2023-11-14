06cfc6b354bd0cd03b0fd0a724025ca3cfe89a36
kyoo0115 <internetkyoomin@gmail.com>
2023-11-14 16:32:49 +0900

    feat: implemented additional features for the validation of date input

--- src/main/java/christmas/model/validator/DateValidator.java
+++ src/main/java/christmas/model/validator/DateValidator.java
@@ -10,7 +10,7 @@ public class DateValidator implements Validator<Integer>{
             }
             return date;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 }
