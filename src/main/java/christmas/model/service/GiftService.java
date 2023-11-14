696cddc79d67a88c69d1f3b08020380c0727d719
kyoo0115 <internetkyoomin@gmail.com>
2023-11-15 01:54:02 +0900

    feat: changed boolean to void for eligibility

--- src/main/java/christmas/model/service/GiftService.java
+++ src/main/java/christmas/model/service/GiftService.java
@@ -1,18 +1,17 @@
 package christmas.model.service;
 
-import christmas.model.Order;
+import christmas.model.entity.Order;
 import java.util.Optional;
 
 public class GiftService {
     public static final long GIFT_ELIGIBILITY_THRESHOLD = 120_000;
     public static final long GIFT_VALUE = 25_000;
-    public static final String GIFT_ITEM = "샴폐인";
+    public static final String GIFT_ITEM = "샴페인";
 
-    public boolean checkGiftEligibility(Order order) {
-        long totalPriceBeforeDiscounts = order.calculateTotalPriceBeforeDiscounts();
-        boolean isEligibleGift = totalPriceBeforeDiscounts >= GIFT_ELIGIBILITY_THRESHOLD;
-        order.setGiftEligibility(isEligibleGift);
-        return isEligibleGift;
+    public void processGiftEligibility(Order order) {
+        long totalPriceBeforeDiscounts = order.calculateTotalPrice();
+        boolean isEligible = totalPriceBeforeDiscounts >= GIFT_ELIGIBILITY_THRESHOLD;
+        order.updateGiftEligibility(isEligible);
     }
 
     public Optional<String> getGiftItem(Order order) {
