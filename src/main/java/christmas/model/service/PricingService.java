a13366510d88aa9076d2fa9747d01da53c3e70db
kyoo0115 <internetkyoomin@gmail.com>
2023-11-15 01:53:22 +0900

    feat: added pricing service to calculate the total price

--- src/main/java/christmas/model/service/PricingService.java
+++ src/main/java/christmas/model/service/PricingService.java
@@ -1,2 +1,15 @@
-package christmas.model.service;public class PricingService {
+package christmas.model.service;
+
+import christmas.model.entity.MenuItem;
+import christmas.model.entity.Order;
+
+import java.util.Map;
+
+public class PricingService {
+
+    public long calculateTotalPrice(Map<MenuItem, Integer> items) {
+        return items.entrySet().stream()
+                .mapToLong(entry -> (long) entry.getKey().price() * entry.getValue())
+                .sum();
+    }
 }
