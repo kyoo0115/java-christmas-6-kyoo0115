762870c4dec7d744c663e2526f1d81496e564366
kyoo0115 <internetkyoomin@gmail.com>
2023-11-15 01:50:08 +0900

    feat: edited magic number to constant

--- src/main/java/christmas/model/entity/MenuItem.java
+++ src/main/java/christmas/model/entity/MenuItem.java
@@ -1,5 +1,7 @@
-package christmas.model;
+package christmas.model.entity;
 
+import christmas.model.MenuCategory;
+import christmas.utils.Constants;
 import java.util.Objects;
 
 public record MenuItem(String name, int price, MenuCategory category) {
@@ -11,13 +13,9 @@ public record MenuItem(String name, int price, MenuCategory category) {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("Item name cannot be null or empty.");
         }
-        if (price < 0) {
+        if (price < Constants.ZERO) {
             throw new IllegalArgumentException("Item price cannot be negative.");
         }
         Objects.requireNonNull(category, "Item category cannot be null.");
-    }
-
-    public String getName() {
-        return name;
     }
 }
