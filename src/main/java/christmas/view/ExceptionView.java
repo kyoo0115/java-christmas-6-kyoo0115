9326c8842e1e25683d3ba541117a04c2476a7727
kyoo0115 <internetkyoomin@gmail.com>
2023-11-15 01:55:08 +0900

    feat: implemented enum for exceptionMessage

--- src/main/java/christmas/view/ExceptionView.java
+++ src/main/java/christmas/view/ExceptionView.java
@@ -1,4 +1,18 @@
 package christmas.view;
 
-public class ExceptionView {
+public enum ExceptionView {
+    INVALID_MENU_ITEM("[ERROR] 메뉴판에 없는 메뉴를 입력하셨습니다. 다시 입력해 주세요."),
+    INVALID_ORDER_FORMAT("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_QUANTITY("[ERROR] 메뉴의 개수는 1 이상이어야 합니다. 다시 입력해 주세요."),
+    DUPLICATE_MENU_ITEM("[ERROR] 중복 메뉴를 입력하셨습니다. 다시 입력해 주세요.");
+
+    private final String message;
+    ExceptionView(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
